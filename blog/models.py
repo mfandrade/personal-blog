7ee0b2eb813212df2ac67ea2e3889ffe0a82684a
Marcelo F Andrade <mfandrade@gmail.com>
2025-01-20 15:52:55 -0300

    feat: create models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now_add=True)
+    categories = models.ManyToManyField("Category", related_name="posts")
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Comment(models.Model):
+    author = models.CharField(max_length=60)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey("Post", on_delete=models.CASCADE)
