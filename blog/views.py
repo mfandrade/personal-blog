35113ec6d4c46f5b45105c952f79dae8b854edf4
Marcelo F Andrade <mfandrade@gmail.com>
2025-01-20 16:30:41 -0300

    feat: post views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from blog.models import Post, Comment
 
-# Create your views here.
+
+def post_index(request):
+    posts = Post.objects.all().order_by("-created")
+    context = {
+        "posts": posts,
+    }
+
+    return render(request, "post/index.html", context)
+
+
+def post_category(request, category):
+    posts = Post.objects.filter(
+        categories__name__contains=category,
+    ).order_by("-created")
+
+    context = {
+        "category": category,
+        "posts": posts,
+    }
+
+    return render(request, "post/category.html", context)
+
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    comments = Comment.objects.filter(post=post)
+    context = {
+        "post": post,
+        "comments": comments,
+    }
+
+    return render(request, "post/detail.html", context)
