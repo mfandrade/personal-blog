27570ab29a0e5384914256f3bd166d1a0bce9418
Marcelo F Andrade <mfandrade@gmail.com>
2025-01-20 18:53:39 -0300

    feat: support for postgres

--- main/settings.py
+++ main/settings.py
@@ -78,9 +78,17 @@ WSGI_APPLICATION = "main.wsgi.application"
 
 DATABASES = {
     "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "blog",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",
+        "PORT": "5432",
+    },
+    "local": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
-    }
+    },
 }
 
 
