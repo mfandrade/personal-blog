984a15cdb4cfe29fbfb392abb46840be6a49b10d
Marcelo F Andrade <mfandrade@gmail.com>
2025-01-20 16:01:52 -0300

    feat: str representation for models

--- blog/models.py
+++ blog/models.py
@@ -8,9 +8,18 @@ class Post(models.Model):
     modified = models.DateTimeField(auto_now_add=True)
     categories = models.ManyToManyField("Category", related_name="posts")
 
+    def __str__(self) -> str:
+        return self.title
+
 
 class Category(models.Model):
     name = models.CharField(max_length=30)
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.name
 
 
 class Comment(models.Model):
@@ -18,3 +27,6 @@ class Comment(models.Model):
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     post = models.ForeignKey("Post", on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"{self.author} on {self.post}"
