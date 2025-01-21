9cba3bca0a1ab1a5edef15f87591796765bd31da
Marcelo F Andrade <mfandrade@gmail.com>
2025-01-21 10:49:32 -0300

    docs: instruction comments in script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
-#
 # Este script contém os passos para se ter a aplicação rodando do zero.
+# Se você já clonou este repositório localmente seu conteúdo é redundante.
+# Neste caso, cada vez que quiser ter a aplicação rodando no seu ambiente
+# de desenvolvimento apenas execute:
 #
+# make
+
+# Clone
 git clone https://github.com/mfandrade/personal-blog
 cd personal-blog
+
+# Selecione a versão
+git checkout v1.0.0
+
+# Execute
 docker compose up --build --remove-orphans
-if test -f .env.local; then
-  www-browser http://localhost:8000
-fi
+
+echo '[INFO] Aplicação disponível em http://localhost:8080'
